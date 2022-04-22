fab858541466777d789c510fceba74263352eec9
mativaclub <96348077+mativaclub@users.noreply.github.com>
2022-04-22 21:45:39 +0300

    calculator testing homework

---
+++ src/main/java/com/calculatortesting/calculatortesting/CalculatorServiceImplParameterTest.java
@@ -0,0 +1,52 @@
+package com.calculatortesting.calculatortesting;
+
+import com.calculatortesting.calculatortesting.service.impl.CalculatorServiceImpl;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static com.calculatortesting.calculatortesting.CalculatorServiceImplTestConstants.ONE;
+import static com.calculatortesting.calculatortesting.CalculatorServiceImplTestConstants.TWO;
+import static org.assertj.core.internal.bytebuddy.implementation.bytecode.StackSize.ZERO;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorServiceImplParameterTest {
+
+    private final CalculatorServiceImpl out = new CalculatorServiceImpl();
+
+    @MethodSource("provideArgumentsForCalculatorTests")
+    @ParameterizedTest
+    public void shouldReturnCorrectResultOfSummation(int num1, int num2) {
+        assertEquals(num1 + num2, out.plus(num1, num2));
+    }
+
+    @MethodSource("provideArgumentsForCalculatorTests")
+    @ParameterizedTest
+    public void shouldReturnCorrectResultOfSubtraction(int num1, int num2) {
+        assertEquals(num1 - num2, out.minus(num1, num2));
+    }
+
+    @MethodSource("provideArgumentsForCalculatorTests")
+    @ParameterizedTest
+    public void shouldReturnCorrectResultOfMultiplication(int num1, int num2) {
+        assertEquals(num1 * num2, out.multiply(num1, num2));
+    }
+
+    @MethodSource("provideArgumentsForCalculatorTests")
+    @ParameterizedTest
+    public void shouldReturnCorrectResultOfDivision(int num1, int num2) {
+        assertEquals(num1 / num2, out.divide(num1, num2));
+    }
+
+    public static Stream<Arguments> provideArgumentsForCalculatorTests() {
+        return Stream.of(
+                Arguments.of(ZERO, ONE),
+                Arguments.of(ZERO, TWO),
+                Arguments.of(ONE, ONE),
+                Arguments.of(ONE, TWO),
+                Arguments.of(TWO, TWO)
+        );
+    }
+}
---
+++ src/main/java/com/calculatortesting/calculatortesting/CalculatorServiceImplTest.java
@@ -0,0 +1,69 @@
+package com.calculatortesting.calculatortesting;
+
+import com.calculatortesting.calculatortesting.exceptions.DivideByZeroException;
+import com.calculatortesting.calculatortesting.service.impl.CalculatorServiceImpl;
+import org.junit.jupiter.api.Test;
+
+import static com.calculatortesting.calculatortesting.CalculatorServiceImplTestConstants.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CalculatorServiceImplTest {
+
+    private final CalculatorServiceImpl out = new CalculatorServiceImpl();
+
+    @Test
+    public void shouldReturn3WhenPlus1and2() {
+        assertEquals(THREE, out.plus(ONE, TWO));
+    }
+
+
+    @Test
+    public void shouldReturn2WhenPlus0and2() {
+        assertEquals(TWO, out.plus(ZERO, TWO));
+    }
+
+
+    @Test
+    public void shouldReturn0WhenMinus2and2() {
+        assertEquals(ZERO, out.minus(TWO, TWO));
+    }
+
+
+    @Test
+    public void shouldReturn1WhenMinus2and1() {
+        assertEquals(ONE, out.minus(TWO, ONE));
+    }
+
+
+    @Test
+    public void shouldReturn1WhenMultiply1and1() {
+        assertEquals(ONE, out.multiply(ONE, ONE));
+    }
+
+
+    @Test
+    public void shouldReturn2WhenMultiply2and1() {
+        assertEquals(TWO, out.multiply(TWO, ONE));
+    }
+
+
+    @Test
+    public void shouldReturn1WhenDivide2and2() {
+        assertEquals(ONE, out.divide(TWO, TWO));
+    }
+
+
+    @Test
+    public void shouldReturn2WhenDivide2and1() {
+        assertEquals(TWO, out.divide(TWO, ONE));
+    }
+
+    @Test
+    public void shouldThrowDivideByZeroExceptionWhenDivideBy0() {
+        assertThrows(DivideByZeroException.class, () -> out.divide(TWO, ZERO));
+    }
+
+
+
+}
